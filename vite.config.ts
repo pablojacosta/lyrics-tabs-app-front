e6e6028235b17b2ad04a8e6e7d75f743eb6ce67c
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-09-22 09:04:52 -0300

    feat: :wrench: Modified vite.config and gitignore.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
+
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +26,6 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
+  define: viteEnv,
   plugins: [react(), tsconfigPaths()],
-})
+});
